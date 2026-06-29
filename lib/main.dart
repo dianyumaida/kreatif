1a9f311b1b08a1d9b7f3df9c9e496e54fe4fa909
rizkyu <160323231+dianyumaida@users.noreply.github.com>
2026-06-29 13:59:51 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -33,16 +33,19 @@ class _BookViewerState extends State<BookViewer> {
       body: SafeArea(
         child: Center(
           child: Container(
-            aspectRatio: 3 / 4,
             margin: const EdgeInsets.all(12.0),
-            child: PageFlipWidget(
-              key: _controller,
-              backgroundColor: Colors.black,
-              children: <Widget>[
-                _page('File/Gambar/Buku/A.png'),
-                _page('File/Gambar/Buku/B.png'),
-                _page('File/Gambar/Buku/C.png'),
-              ],
+            // Mengatur rasio halaman menggunakan widget AspectRatio yang standar
+            child: AspectRatio(
+              aspectRatio: 3 / 4,
+              child: PageFlipWidget(
+                key: _controller,
+                backgroundColor: Colors.black,
+                children: <Widget>[
+                  _page('File/Gambar/Buku/A.png'),
+                  _page('File/Gambar/Buku/B.png'),
+                  _page('File/Gambar/Buku/C.png'),
+                ],
+              ),
             ),
           ),
         ),
